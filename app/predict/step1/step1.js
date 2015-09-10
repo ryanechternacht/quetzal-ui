b4540f6dcb032eb11b24eaf67b85ffb0f028ec3e
ryanechternacht <ryan@echternacht.org>
2015-09-10 15:54:46 -0400

    Added a sample data file to work with
    Figured out how to wire up a service and controller. I also added
    ngResource to read the json file I created.

--- app/predict/step1/step1.js
+++ app/predict/step1/step1.js
@@ -1,6 +1,11 @@
 'use strict';
 
-angular.module('step1', [])
-.controller('Step1Ctrl', [function() {
-
+angular.module('step1', ['ngResource'])
+.factory('Step1Srv', ['$resource', function($resource) {
+	return $resource('data/:dataset.json', {}, {
+		query: {method:'GET'}
+	});
+}])
+.controller('Step1Ctrl', ['$scope', 'Step1Srv', function($scope, Step1Srv) {
+	$scope.dataset = Step1Srv.get({dataset:'data'});
 }]);
