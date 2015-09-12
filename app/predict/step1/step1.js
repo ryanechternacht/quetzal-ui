d2d0ce0b7a05e85a2095379a7a1882efc3948573
ryanechternacht <ryan@echternacht.org>
2015-09-12 15:19:26 -0400

    Added a column list to step 1 and some binding
    Figuring out in page binding and using controller actions. This is
    beginning to make sense, although I think I’m missing some angular
    magic to make this cleaner

--- app/predict/step1/step1.js
+++ app/predict/step1/step1.js
@@ -3,9 +3,25 @@
 angular.module('step1', ['ngResource'])
 .factory('Step1Srv', ['$resource', function($resource) {
 	return $resource('data/:dataset.json', {}, {
-		query: {method:'GET'}
+		get: {method:'GET'}
 	});
 }])
 .controller('Step1Ctrl', ['$scope', 'Step1Srv', function($scope, Step1Srv) {
-	$scope.dataset = Step1Srv.get({dataset:'data'});
+    
+    $scope.setActiveColumn = function(name) { 
+        for(var c = 0; c < $scope.dataset.columns.length; c++) { 
+            var col = $scope.dataset.columns[c];
+            if(col.name == name) { 
+                col.active = true;
+                $scope.activeColumn = col;
+            }
+            else {
+                col.active = false;
+            }
+        }
+    };
+
+	$scope.dataset = Step1Srv.get({dataset:'data'}, function (data) {
+        $scope.setActiveColumn($scope.dataset.columns[0].name);
+    });
 }]);
