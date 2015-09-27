3078897cbca83a449ba9debaebbebaf9e23b2acf
ryanechternacht <ryan@echternacht.org>
2015-09-27 02:21:38 -0400

    More Charting Improvements
    Still some work to do, but the d3 chart is getting better.

--- app/d3/d3histogram.js
+++ app/d3/d3histogram.js
@@ -12,29 +12,6 @@ var drawHistogram = function(d3, svg, scope, iElement, iAttrs, data) {
 
     var dataset = scope.data;
 
-    // var dataset = [
-    //     {"x": -2.808341779, "y":8},
-    //     {"x": -2.513197563, "y":8},
-    //     {"x": -2.218053347, "y":3},
-    //     {"x": -1.922909131, "y":8},
-    //     {"x": -1.627764915, "y":3},
-    //     {"x": -1.3326207, "y":4},
-    //     {"x": -1.037476484, "y":2},
-    //     {"x": -0.742332268, "y":6},
-    //     {"x": -0.447188052, "y":4},
-    //     {"x": -0.152043836, "y":7},
-    //     {"x": 0.14310038, "y":5},
-    //     {"x": 0.438244596, "y":4},
-    //     {"x": 0.733388812, "y":4},
-    //     {"x": 1.028533028, "y":3},
-    //     {"x": 1.323677244, "y":8},
-    //     {"x": 1.618821459, "y":4},
-    //     {"x": 1.913965675, "y":3},
-    //     {"x": 2.209109891, "y":6},
-    //     {"x": 2.504254107, "y":4},
-    //     {"x": 2.799398323, "y":6}
-    // ];
-
     var barPadding = 5,
         xPadding = 30,
         yPaddingBottom = 30,
@@ -47,54 +24,65 @@ var drawHistogram = function(d3, svg, scope, iElement, iAttrs, data) {
                     .domain([0, d3.max(dataset, function(d) { return d.y; })])
                     .range([yBottom, yTop]);
 
-    svg.selectAll("rect")
+    var xScale = d3.scale.ordinal()
+                    .domain(data.map(function(d) { return d.x; }))
+                    .rangeRoundBands([0, width], .1);
+
+    var bar = svg.selectAll("g")
         .data(dataset)
         .enter()
-        .append("rect")
-        .attr("x", function(d,i) { 
-            return i * (w / dataset.length);
-        })
-        .attr("y", function(d,i) { 
-            return yScale(d.y); 
-        })
-        .attr("width", w / dataset.length - barPadding)
-        .attr("height", function(d,i) { 
-            return yBottom - yScale(d.y); 
-        })
+        .append("g")
+        .attr("transform", function(d) { 
+            return "translate(" + xScale(d.x) + ",0)";
+        });
+
+    bar.append("rect")
+        .attr("y", function(d, i) { return yScale(d.y); })
+        .attr("height", function(d) { return yBottom - yScale(d.y); })
+        .attr("width", function(d) { return xScale.rangeBand(); })
         .attr("class", "bar");
 
-        svg.selectAll("text")
-            .data(dataset)
-            .enter()
-            .append("text")
-            .text(function(d,i) { 
-                return d.y;
-            })
-            .attr("x", function(d,i) { 
-                return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
-            })
-            .attr("y", function(d,i) { 
-                return yScale(d.y) - 5;
-            })
-            .attr("font-family", "sans-serif")
-            .attr("font-fize", "11px")
-            .attr("text-anchor", "middle");
+    bar.append("text")
+        .text(function(d,i) { return d.y; })
+        .attr("y", function(d,i) { return yScale(d.y) - 5; })
+        .attr("x", dataset.length)
+        .attr("width", function(d) { return xScale.rangeBand(); })
+        .attr("font-family", "sans-serif")
+        .attr("font-fize", "11px")
+        .attr("text-anchor", "middle");
 
-        svg.selectAll("g")
-            .data(dataset)
-            .enter()
-            .append("text")
-            .text(function(d,i) { 
-                // from http://stackoverflow.com/questions/6134039/format-number-to-always-show-2-decimal-places
-                return parseFloat(Math.round(d.x * 100) / 100).toFixed(1);
-            })
-            .attr("x", function(d,i) { 
-                return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
-            })
-            .attr("y", yBottom + 15)
-            .attr("font-family", "sans-serif")
-            .attr("font-fize", "11px")
-            .attr("text-anchor", "middle");
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left");
+
+    svg.append("g")
+        .attr("class", "axis x-axis")
+        .attr("transform", "translate(0," + yBottom + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "axis y-axis")
+        .call(yAxis);
+
+    //     svg.selectAll("g")
+    //         .data(dataset)
+    //         .enter()
+    //         .append("text")
+    //         .text(function(d,i) { 
+    //             // from http://stackoverflow.com/questions/6134039/format-number-to-always-show-2-decimal-places
+    //             return parseFloat(Math.round(d.x * 100) / 100).toFixed(1);
+    //         })
+    //         .attr("x", function(d,i) { 
+    //             return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+    //         })
+    //         .attr("y", yBottom + 15)
+    //         .attr("font-family", "sans-serif")
+    //         .attr("font-fize", "11px")
+    //         .attr("text-anchor", "middle");
 }
 
 angular.module('d3').directive('d3Histogram', ['d3', function(d3) { 
